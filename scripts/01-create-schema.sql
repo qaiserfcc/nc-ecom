577991359ddfc13cf877d5e2830d81eee820f0a8
Qaiser Nadeem <qaiserfcc@gmail.com>
2026-01-03 08:26:56 +0500

    schema uodated

--- scripts/01-create-schema.sql
+++ scripts/01-create-schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   email TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
+  password_hash TEXT,
   phone TEXT,
   address TEXT,
   city TEXT,
